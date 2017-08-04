e2304bef4ff8b4f823314edb2162c41bfad25daf
TropComplique <antoshchenko.b@gmail.com>
2017-08-04 21:08:08 +0000

    use squeezenet

---
+++ squeezenet.py
@@ -0,0 +1,96 @@
+import numpy as np
+import keras
+from keras.models import Model
+from keras.layers import Input, Convolution2D, MaxPooling2D
+from keras.layers import Activation, Dropout, GlobalAveragePooling2D, concatenate
+
+
+mean = np.array([0.485, 0.456, 0.406], dtype='float32')
+std = np.array([0.229, 0.224, 0.225], dtype='float32')
+
+
+def preprocess_input(x):
+    x /= 255.0
+    x -= mean
+    x /= std
+    return x
+
+
+# a building block of the SqueezeNet architecture
+def fire_module(number, x, squeeze, expand, weight_decay=None, trainable=False):
+    
+    module_name = 'fire' + number
+    
+    if trainable and weight_decay is not None:
+        kernel_regularizer = keras.regularizers.l2(weight_decay) 
+    else:
+        kernel_regularizer = None
+    
+    x = Convolution2D(
+        squeeze, (1, 1), 
+        name=module_name + '/' + 'squeeze',
+        trainable=trainable, 
+        kernel_regularizer=kernel_regularizer
+    )(x)
+    x = Activation('relu')(x)
+
+    a = Convolution2D(
+        expand, (1, 1),
+        name=module_name + '/' + 'expand1x1',
+        trainable=trainable, 
+        kernel_regularizer=kernel_regularizer
+    )(x)
+    a = Activation('relu')(a)
+
+    b = Convolution2D(
+        expand, (3, 3), padding='same',
+        name=module_name + '/' + 'expand3x3',
+        trainable=trainable, 
+        kernel_regularizer=kernel_regularizer
+    )(x)
+    b = Activation('relu')(b)
+
+    return concatenate([a, b])
+
+
+def SqueezeNet(weight_decay, image_size=224):
+
+    image = Input(shape=(image_size, image_size, 3))
+
+    x = Convolution2D(
+        64, (3, 3), strides=(2, 2), name='conv1', 
+        trainable=False
+    )(image) # 111, 111, 64
+    
+    x = Activation('relu')(x)
+    x = MaxPooling2D(pool_size=(3, 3), strides=(2, 2))(x) # 55, 55, 64
+
+    x = fire_module('2', x, squeeze=16, expand=64) # 55, 55, 128
+    x = fire_module('3', x, squeeze=16, expand=64) # 55, 55, 128
+    x = MaxPooling2D(pool_size=(3, 3), strides=(2, 2))(x) # 27, 27, 128
+
+    x = fire_module('4', x, squeeze=32, expand=128) # 27, 27, 256
+    x = fire_module('5', x, squeeze=32, expand=128) # 27, 27, 256
+    x = MaxPooling2D(pool_size=(3, 3), strides=(2, 2))(x) # 13, 13, 256
+    
+    x = fire_module('6', x, squeeze=48, expand=192) # 13, 13, 384
+    x = fire_module('7', x, squeeze=48, expand=192) # 13, 13, 384
+    x = fire_module('8', x, squeeze=64, expand=256) # 13, 13, 512
+    x = fire_module('9', x, 64, 256, weight_decay, trainable=False) # 13, 13, 512
+    
+    x = Dropout(0.5)(x)
+    x = Convolution2D(
+        256, (1, 1), name='conv10',
+        kernel_initializer=keras.initializers.RandomNormal(stddev=0.01),
+        kernel_regularizer=keras.regularizers.l2(weight_decay)
+    )(x) # 13, 13, 256
+    
+    x = Activation('relu')(x)
+    logits = GlobalAveragePooling2D()(x) # 256
+    probabilities = Activation('softmax')(logits)
+    
+    model = Model(image, probabilities)
+    model.load_weights('squeezenet_weights.hdf5', by_name=True)
+    
+    return model
+
--- xception.py
+++ xception.py
@@ -1,13 +1,130 @@
-from keras.applications.xception import Xception
+import keras
 from keras.models import Model
+from keras.layers import Input, BatchNormalization, Conv2D, SeparableConv2D, MaxPooling2D
 from keras.layers import GlobalAveragePooling2D, Dense, Activation, Dropout
+from keras import layers
+from keras.utils.data_utils import get_file
 
 
-def get_xception():
-    base_model = Xception(include_top=False, weights='imagenet', input_shape=(299, 299, 3))
-    x = base_model.output
-    x = GlobalAveragePooling2D()(x)
+TF_WEIGHTS_PATH_NO_TOP = 'https://github.com/fchollet/deep-learning-models/releases/download/v0.4/xception_weights_tf_dim_ordering_tf_kernels_notop.h5'
+
+
+def preprocess_input(x):
+    x /= 255.0
+    x -= 0.5
+    x *= 2.0
+    return x
+
+
+def Xception(weight_decay=1e-4, classes=256, input_shape=(299, 299, 3)):
+    
+    img_input = Input(shape=input_shape)
+
+    x = Conv2D(32, (3, 3), strides=(2, 2), use_bias=False, name='block1_conv1')(img_input)
+    x = BatchNormalization(name='block1_conv1_bn')(x)
+    x = Activation('relu', name='block1_conv1_act')(x)
+    x = Conv2D(64, (3, 3), use_bias=False, name='block1_conv2')(x)
+    x = BatchNormalization(name='block1_conv2_bn')(x)
+    x = Activation('relu', name='block1_conv2_act')(x)
+
+    residual = Conv2D(128, (1, 1), strides=(2, 2), padding='same', use_bias=False)(x)
+    residual = BatchNormalization()(residual)
+
+    x = SeparableConv2D(128, (3, 3), padding='same', use_bias=False, name='block2_sepconv1')(x)
+    x = BatchNormalization(name='block2_sepconv1_bn')(x)
+    x = Activation('relu', name='block2_sepconv2_act')(x)
+    x = SeparableConv2D(128, (3, 3), padding='same', use_bias=False, name='block2_sepconv2')(x)
+    x = BatchNormalization(name='block2_sepconv2_bn')(x)
+
+    x = MaxPooling2D((3, 3), strides=(2, 2), padding='same', name='block2_pool')(x)
+    x = layers.add([x, residual])
+
+    residual = Conv2D(256, (1, 1), strides=(2, 2), padding='same', use_bias=False)(x)
+    residual = BatchNormalization()(residual)
+
+    x = Activation('relu', name='block3_sepconv1_act')(x)
+    x = SeparableConv2D(256, (3, 3), padding='same', use_bias=False, name='block3_sepconv1')(x)
+    x = BatchNormalization(name='block3_sepconv1_bn')(x)
+    x = Activation('relu', name='block3_sepconv2_act')(x)
+    x = SeparableConv2D(256, (3, 3), padding='same', use_bias=False, name='block3_sepconv2')(x)
+    x = BatchNormalization(name='block3_sepconv2_bn')(x)
+
+    x = MaxPooling2D((3, 3), strides=(2, 2), padding='same', name='block3_pool')(x)
+    x = layers.add([x, residual])
+
+    residual = Conv2D(728, (1, 1), strides=(2, 2), padding='same', use_bias=False)(x)
+    residual = BatchNormalization()(residual)
+
+    x = Activation('relu', name='block4_sepconv1_act')(x)
+    x = SeparableConv2D(728, (3, 3), padding='same', use_bias=False, name='block4_sepconv1')(x)
+    x = BatchNormalization(name='block4_sepconv1_bn')(x)
+    x = Activation('relu', name='block4_sepconv2_act')(x)
+    x = SeparableConv2D(728, (3, 3), padding='same', use_bias=False, name='block4_sepconv2')(x)
+    x = BatchNormalization(name='block4_sepconv2_bn')(x)
+
+    x = MaxPooling2D((3, 3), strides=(2, 2), padding='same', name='block4_pool')(x)
+    x = layers.add([x, residual])
+
+    for i in range(8):
+        residual = x
+        prefix = 'block' + str(i + 5)
+
+        x = Activation('relu', name=prefix + '_sepconv1_act')(x)
+        x = SeparableConv2D(728, (3, 3), padding='same', use_bias=False, name=prefix + '_sepconv1')(x)
+        x = BatchNormalization(name=prefix + '_sepconv1_bn')(x)
+        x = Activation('relu', name=prefix + '_sepconv2_act')(x)
+        x = SeparableConv2D(728, (3, 3), padding='same', use_bias=False, name=prefix + '_sepconv2')(x)
+        x = BatchNormalization(name=prefix + '_sepconv2_bn')(x)
+        x = Activation('relu', name=prefix + '_sepconv3_act')(x)
+        x = SeparableConv2D(728, (3, 3), padding='same', use_bias=False, name=prefix + '_sepconv3')(x)
+        x = BatchNormalization(name=prefix + '_sepconv3_bn')(x)
+
+        x = layers.add([x, residual])
+
+    residual = Conv2D(1024, (1, 1), strides=(2, 2), padding='same', use_bias=False)(x)
+    residual = BatchNormalization()(residual)
+
+    x = Activation('relu', name='block13_sepconv1_act')(x)
+    x = SeparableConv2D(728, (3, 3), padding='same', use_bias=False, name='block13_sepconv1')(x)
+    x = BatchNormalization(name='block13_sepconv1_bn')(x)
+    x = Activation('relu', name='block13_sepconv2_act')(x)
+    x = SeparableConv2D(1024, (3, 3), padding='same', use_bias=False, name='block13_sepconv2')(x)
+    x = BatchNormalization(name='block13_sepconv2_bn')(x)
+
+    x = MaxPooling2D((3, 3), strides=(2, 2), padding='same', name='block13_pool')(x)
+    x = layers.add([x, residual])
+
+    x = SeparableConv2D(1536, (3, 3), padding='same', use_bias=False, name='block14_sepconv1')(x)
+    x = BatchNormalization(name='block14_sepconv1_bn')(x)
+    x = Activation('relu', name='block14_sepconv1_act')(x)
+
+    x = SeparableConv2D(
+        2048, (3, 3), padding='same', 
+        use_bias=False, name='block14_sepconv2',
+        depthwise_regularizer=keras.regularizers.l2(weight_decay),
+        pointwise_regularizer=keras.regularizers.l2(weight_decay)
+    )(x)
+    x = BatchNormalization(name='block14_sepconv2_bn')(x)
+    x = Activation('relu', name='block14_sepconv2_act')(x)
+    
+    x = GlobalAveragePooling2D(name='avg_pool')(x)
+    model = Model(img_input, x, name='xception')
+    
+    # load weights
+    weights_path = get_file(
+        'xception_weights_tf_dim_ordering_tf_kernels_notop.h5',
+        TF_WEIGHTS_PATH_NO_TOP, cache_subdir='models'
+    )
+    model.load_weights(weights_path)
+    
+    x = model.output
     x = Dropout(0.5)(x)
-    logits = Dense(256)(x)
+    logits = Dense(classes, kernel_regularizer=keras.regularizers.l2(weight_decay))(x)
     probabilities = Activation('softmax')(logits)
-    return Model(base_model.input, probabilities)
+
+    model = Model(model.input, probabilities, name='xception')
+    
+    for layer in model.layers[:-7]:
+        layer.trainable = False
+
+    return model
